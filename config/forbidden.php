bdb60bb51ea62b455fee5d6ce3b4be213bd74d30
khaydarov <murod.haydarov@inbox.ru>
2016-07-14 13:19:55 +0300

    v1.0.0

---
+++ config/forbidden.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Alias System
+ * https://ifmo.su/
+ * @author CodeX team team@ifmo.su
+ */
+
+    return array(
+        'system' => array(
+            'auth',
+            'articles',
+            'article',
+            'contests',
+            'contest',
+            'admin',
+        )
+    )
+
+?>
